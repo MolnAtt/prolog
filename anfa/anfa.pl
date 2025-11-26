d159db23f7469818361680426fb5c48905d44f48
Molnár Attila <molnar.attila@szlgbp.hu>
2025-11-26 19:19:35 +0100

    DFC, model simplification

--- anfa/anfa.pl
+++ anfa/anfa.pl
@@ -34,17 +34,33 @@ semantic_tableaux([lnot(F)|_], Po, _, closed, []):-
 % Recursive clauses %
 %%%%%%%%%%%%%%%%%%%%%
 
-% 2A: negation of positive literal does not occur in known negative literals, so we move it to known positive literals
+% 2AA: negation of positive literal does not occur in known negative literals, so we move it to known positive literals
 semantic_tableaux([F|Formulas], Po, Ne, State, Models):- 
     atomic_formula(F),
     does_not_occur_in(lnot(F), Ne),
+    does_not_occur_in(F, Po),
     semantic_tableaux(Formulas, [F|Po], Ne, State, Models).
 
-% 2A: the atom of negative literal does not occur in known positive literals, so we move it to known negative literals
+% 2AB: negation of positive literal does not occur in known negative literals, so we move it to known positive literals
+semantic_tableaux([F|Formulas], Po, Ne, State, Models):- 
+    atomic_formula(F),
+    does_not_occur_in(lnot(F), Ne),
+    member(F, Po),
+    semantic_tableaux(Formulas, Po, Ne, State, Models).
+
+% 2BA: the atom of negative literal does not occur in known positive literals, so we move it to known negative literals
 semantic_tableaux([lnot(F)|Formulas], Po, Ne, State, Models):- 
     atomic_formula(F),
     does_not_occur_in(F, Po),
+    does_not_occur_in(lnot(F), Ne),
     semantic_tableaux(Formulas, Po, [lnot(F)|Ne], State, Models).
+
+% 2BB: the atom of negative literal does not occur in known positive literals, so we move it to known negative literals
+semantic_tableaux([lnot(F)|Formulas], Po, Ne, State, Models):- 
+    atomic_formula(F),
+    does_not_occur_in(F, Po),
+    member(lnot(F), Ne),
+    semantic_tableaux(Formulas, Po, Ne, State, Models).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 3: non-branching connectives: %
@@ -107,3 +123,57 @@ combine_or(closed, [], open, Models2, open, Models2).
 combine_or(open, Models1, open, Models2, open, Models) :-
     append(Models1, Models2, Models).
 
+
+
+%%%%%%%%%%%%%%%%%%%
+% INFIX -> PREFIX %
+%%%%%%%%%%%%%%%%%%%
+
+% Belépési pont
+parse_formula(String, Formula) :-
+    string_chars(String, Chars),
+    phrase(expr(Formula), Chars).
+
+% =  (ekvivalencia)
+expr(F) --> impl(F1), ['='], expr(F2), {F = liff(F1,F2)}.
+expr(F) --> impl(F).
+
+% >  (implikáció)
+impl(F) --> disj(F1), ['>'], impl(F2), {F = lthen(F1,F2)}.
+impl(F) --> disj(F).
+
+% |  (diszjunkció)
+disj(F) --> conj(F1), ['|'], disj(F2), {F = lor(F1,F2)}.
+disj(F) --> conj(F).
+
+% &  (konjunkció)
+conj(F) --> neg(F1), ['&'], conj(F2), {F = land(F1,F2)}.
+conj(F) --> neg(F).
+
+% !  (negáció)
+neg(F) --> ['!'], neg(F1), {F = lnot(F1)}.
+neg(F) --> atom(F).
+
+%%%%%%%%%%%%%%%%%%%%%%%
+% Atomok és zárójelek %
+%%%%%%%%%%%%%%%%%%%%%%%
+
+atom(F) --> ['('], expr(F), [')'].
+atom(F) --> [C], {atomic_formula(C), F = C}.
+
+
+find_models(Formulastring, State, UniqueModels):- parse_formula(Formulastring, Formula), semantic_tableaux([Formula], State, Models), parok_osszevonasa(Models, SimpleModels), unique_sets(SimpleModels, UniqueModels).
+
+
+parok_osszevonasa([], []).
+parok_osszevonasa([[Po, Ne]|T], [M|MT]):- 
+    append(Po, Ne, M),
+    parok_osszevonasa(T, MT).
+
+unique_sets(ListOfLists, Unique) :-
+    maplist(sort, ListOfLists, Normalized),
+    sort(Normalized, Unique).
+
+
+% query: 
+% find_models('!(!(a&b)=(!a|b))', State, Models).
